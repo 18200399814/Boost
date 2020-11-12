a1e4bab7466d67cf2914bb3d271eaef8701d6ca5
18200399814 <2272203582@qq.com>
2020-11-13 00:05:09 +0800

    使用VS2017社区版编译boost库，并编写一个boost库版本的helloworld程序，以测试boost库编译安装是否成功。

---
+++ example/helloboost/main.cpp
@@ -0,0 +1,28 @@
+﻿// helloboost.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+#include <boost/thread/thread.hpp>
+
+static void sayHello();
+
+int main(void)
+{
+	std::cout << BOOST_VERSION << std::endl;
+	std::cout << BOOST_LIB_VERSION << std::endl;
+	std::cout << BOOST_PLATFORM << std::endl;
+	std::cout << BOOST_COMPILER << std::endl;
+	std::cout << BOOST_STDLIB << std::endl;
+
+	boost::thread my_thread(&sayHello);
+	my_thread.join();
+
+	return 0;
+}
+
+void sayHello()
+{
+	std::cout << "Hello World!\n";
+}
